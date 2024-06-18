6ccfdbc4338f213e3838d1ea3afe9ffb5191ad35
SakhavalaRavi <107946621+SakhavalaRavi@users.noreply.github.com>
2024-06-18 21:59:31 +0530

    Add files via upload

---
+++ finalExample/ChildClass.java
@@ -0,0 +1,10 @@
+package finalExample;
+
+public class ChildClass extends FinalVariable
+{
+	@Override
+	public final void show()
+	{
+		System.out.println("I am final function :) ");
+	}
+}
---
+++ finalExample/FinalVariable.java
@@ -0,0 +1,22 @@
+package finalExample;
+
+final class Test
+{
+	
+}
+
+public class FinalVariable 
+{
+	final int salary;
+	
+	public FinalVariable()
+	{
+		salary = 145;
+	}
+	
+	public void show()
+	{
+		//Test te = new Test();
+		System.out.println("I am final Function");
+	}
+}
