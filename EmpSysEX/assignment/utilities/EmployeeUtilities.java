dc6031da2c5560085d9af9466e54a9d5c5a51053
SakhavalaRavi <107946621+SakhavalaRavi@users.noreply.github.com>
2024-06-18 21:57:26 +0530

    Add files via upload

---
+++ EmpSysEX/assignment/utilities/EmployeeUtilities.java
@@ -0,0 +1,17 @@
+package assignment.utilities;
+import assignment.employees.*;
+
+public class EmployeeUtilities 
+{
+	public static void printEmployeeDetails(Employee employee)
+	{
+		System.out.println("Name: " + employee.getName());
+		System.out.println("Employee ID: " + employee.getEmployeeId());
+		System.out.println("Salary: " + employee.getSalary());
+	}
+	
+	public static void updateEmployeeSalary(Employee employee, double newSalary)
+	{
+		employee.setSalary(newSalary);
+	}
+}
