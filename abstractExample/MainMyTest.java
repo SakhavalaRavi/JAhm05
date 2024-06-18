5a5e3f2ebfa3ae27f07ab35d42f7d94f63c5aa6a
SakhavalaRavi <107946621+SakhavalaRavi@users.noreply.github.com>
2024-06-18 21:52:48 +0530

    Add files via upload

---
+++ abstractExample/MainMyTest.java
@@ -0,0 +1,14 @@
+package abstractExample;
+
+public class MainMyTest {
+
+	public static void main(String[] args) 
+	{
+		//MyTest t = new MyTest();
+		Addition ad = new Addition();
+		ad.show();
+		ad.getCalculation();
+
+	}
+
+}
---
+++ abstractExample/MyTest.java
@@ -0,0 +1,13 @@
+package abstractExample;
+
+// Cannot create object of class
+public abstract class MyTest 
+{
+	public void show()
+	{
+		System.out.println("I am Abstract Class");
+	}
+	
+	// Can not give function body
+	public abstract void getCalculation();
+}
